95702d442099c00783e64539aeea92749fed5bd1
ywonchae1 <ywonchae62@gmail.com>
2023-09-15 12:38:01 +0900

    홈 화면 템플릿 추가

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const template = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>WEB</h1>
+        <ol>
+            <li>Node.js</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ol>
+        <a href="/post/create">create</a>
+    </body>
+    </html>
+    `
+    res.send(template);
 });
 
 app.listen(port, () => {
